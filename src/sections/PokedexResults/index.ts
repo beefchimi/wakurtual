1fea1753a29153cc13ab3e5f1db9226245d08fdb
beefchimi <curtdulmage@gmail.com>
2024-03-27 16:21:01 -0400

    :truck: [PokedexResults] Move back to sections

---
+++ src/sections/PokedexResults/index.ts
@@ -0,0 +1,5 @@
+export {PokedexResults, type PokedexResultsProps} from './PokedexResults.js';
+export {
+  PokedexResultsSkeleton,
+  type PokedexResultsSkeletonProps,
+} from './PokedexResultsSkeleton.js';
